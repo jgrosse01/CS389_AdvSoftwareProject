be8ed1869bc2be3210f5ed21f7fc6a846e33fe4e
Jake Grosse <jgrosse@carroll.edu>
2023-01-27 12:27:28 -0700

    Initial commit

---
+++ src/main/kotlin/edu/carroll/cs389/Cs389ProjectApplication.kt
@@ -0,0 +1,11 @@
+package edu.carroll.cs389
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Cs389ProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<Cs389ProjectApplication>(*args)
+}
