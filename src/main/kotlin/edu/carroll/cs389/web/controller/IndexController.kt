5b68b2713a02dfd0fd2396eb47ad2c8b68faa005
jgrosse01 <jgrosse@carroll.edu>
2023-02-07 21:55:21 -0700

    ops: added logging for main landing page

--- src/main/kotlin/edu/carroll/cs389/web/controller/IndexController.kt
+++ src/main/kotlin/edu/carroll/cs389/web/controller/IndexController.kt
@@ -1,13 +1,19 @@
 package edu.carroll.cs389.web.controller
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class IndexController {
+    companion object{
+        val log: Logger = LoggerFactory.getLogger(IndexController::class.java)
+    }
+
     @GetMapping("/")
     fun index(): String {
-        
+        log.info("index(): User landed at index page (\"/\")")
         return "index"
     }
 }
