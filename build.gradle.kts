f8f7025748af15a290702f91e883c90bf8f827e4
Jake Grosse <jgrosse@carroll.edu>
2023-02-22 11:40:01 -0700

    ops: added jakarta persistence

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("jakarta.validation:jakarta.validation-api")
+	implementation("jakarta.persistence:jakarta.persistence-api")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
