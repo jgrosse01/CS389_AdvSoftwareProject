a7cad5b2de3b6a1031ecd3f19d432c274af290d6
Jake Grosse <jgrosse@carroll.edu>
2023-02-25 14:08:40 -0700

    feat: added tracker to index page

--- src/main/kotlin/edu/carroll/cs389/web/controller/IndexController.kt
+++ src/main/kotlin/edu/carroll/cs389/web/controller/IndexController.kt
@@ -1,19 +1,22 @@
 package edu.carroll.cs389.web.controller
 
+import edu.carroll.cs389.service.TrackServiceRaw
+import jakarta.servlet.http.HttpServletRequest
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class IndexController {
+class IndexController(private val trackService: TrackServiceRaw) {
     companion object{
         val log: Logger = LoggerFactory.getLogger(IndexController::class.java)
     }
 
     @GetMapping("/")
-    fun index(): String {
+    fun index(req: HttpServletRequest): String {
         log.info("index(): User landed at index page ('/')")
+        trackService.trackClient(req, "/")
         return "index"
     }
 }
