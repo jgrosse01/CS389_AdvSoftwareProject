178f46c92508544eb9974fe641ea7b2c52d73731
Jake Grosse <jgrosse@carroll.edu>
2023-03-10 00:58:48 -0700

    test: attempting to get HttpServletRequests working properly in testing

--- src/test/kotlin/edu/carroll/cs389/service/tracker/TrackServiceTest.kt
+++ src/test/kotlin/edu/carroll/cs389/service/tracker/TrackServiceTest.kt
@@ -1,12 +1,12 @@
 package edu.carroll.cs389.service.tracker
 
-import org.springframework.test.util.AssertionErrors.assertFalse
-import org.springframework.test.util.AssertionErrors.assertNotNull
-import org.springframework.test.util.AssertionErrors.assertTrue
+import jakarta.servlet.http.HttpServletRequest
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
+import org.mockito.Mockito.*
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.test.util.AssertionErrors.assertTrue
+
 
 @SpringBootTest
 class TrackServiceTest(private val trackService: TrackService) {
@@ -14,87 +14,91 @@ class TrackServiceTest(private val trackService: TrackService) {
     // Tests to handle expected input (Happy Path)
     @Test
     fun trackClientTestValidUserAgent() {
-        val mhsr: MockHttpServletRequest = MockHttpServletRequest()
-        mhsr.setParameter(
-            "User-Agent", "Product/1.0 (OperatingSystem 2.0; Plat; bit; randomInfoBcWhyNot) Base/4.0 Browser/5.0"
-        )
-        mhsr.requestURI = "/"
-        mhsr.remoteAddr = "192.168.1.1"
+//        val mockRequest: HttpServletRequest = mock(HttpServletRequest::class.java)
+//
+//        `when`(mockRequest.getHeader("User-Agent")).thenReturn("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
+//        `when`(mockRequest.getParameter("requestURI")).thenReturn("/")
+//        `when`(mockRequest.getParameter("requestAddr")).thenReturn("192.168.1.1")
 
-        assertTrue("trackClientTestValidUserAgent(): should succeed with valid user agent", trackService.trackClient(mhsr))
+        val mockRequest: MockHttpServletRequest = MockHttpServletRequest()
+        mockRequest.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
+        mockRequest.requestURI = "/"
+        mockRequest.remoteAddr = "192.168.1.1"
+
+        assertTrue("trackClientTestValidUserAgent(): should succeed with valid user agent", trackService.trackClient(mockRequest))
     }
 
-    @Test
-    fun trackClientTestValidUserAgentMissingBrowserVersion() {
-
-    }
-
-    @Test
-    fun trackClientTestValidUserAgentMissingOSVersion() {
-
-    }
-
-    @Test
-    fun trackClientTestValidUserAgentMissingOS() {
-
-    }
-
-    @Test
-    fun trackClientTestValidUserAgentMissingBrowser() {
-
-    }
-
-    @Test
-    fun trackClientTestValidUserAgentMissingBrowserAndVersion() {
-
-    }
-
-    @Test
-    fun trackClientTestValidUserAgentMissingOSAndVersion() {
-
-    }
-
-    @Test
-    fun trackClientTestValidUserAgentMissingOSAndVersionAndBrowserVersion() {
-
-    }
-
-    @Test
-    fun trackClientTestValidUserAgentMissingBrowserAndVersionAndOSVersion() {
-
-    }
-
-    // Tests to handle unexpected input (Crappy Path)
-
-    @Test
-    fun trackClientTestMissingUserAgent() {
-
-    }
-
-    @Test
-    fun trackClientTestBrowserAndOSReversedUserAgent() {
-
-    }
-
-    // Tests to handle either missing or malicious input (Crazy Path)
-
-    @Test
-    fun trackClientTestNullUserAgent() {
-
-    }
-
-    @Test
-    fun trackClientTestBlankServletRequest() {
-
-    }
-
-    @Test
-    fun trackClientTestEmptyServletRequest() {
-
-    }
-
-    @Test
-    fun trackClientTestSQLInjectionUserAgent() {
-
-    }
+//    @Test
+//    fun trackClientTestValidUserAgentMissingBrowserVersion() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestValidUserAgentMissingOSVersion() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestValidUserAgentMissingOS() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestValidUserAgentMissingBrowser() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestValidUserAgentMissingBrowserAndVersion() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestValidUserAgentMissingOSAndVersion() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestValidUserAgentMissingOSAndVersionAndBrowserVersion() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestValidUserAgentMissingBrowserAndVersionAndOSVersion() {
+//
+//    }
+//
+//    // Tests to handle unexpected input (Crappy Path)
+//
+//    @Test
+//    fun trackClientTestMissingUserAgent() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestBrowserAndOSReversedUserAgent() {
+//
+//    }
+//
+//    // Tests to handle either missing or malicious input (Crazy Path)
+//
+//    @Test
+//    fun trackClientTestNullUserAgent() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestBlankServletRequest() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestEmptyServletRequest() {
+//
+//    }
+//
+//    @Test
+//    fun trackClientTestSQLInjectionUserAgent() {
+//
+//    }
 }
