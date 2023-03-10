e84f56870ccc2bfeda3b9506b549fc0fa78b2e43
Jake Grosse <jgrosse@carroll.edu>
2023-03-10 14:49:06 -0700

    test: refactored test to reflect change of service, test works, more to come

--- src/test/kotlin/edu/carroll/cs389/service/tracker/TrackServiceTest.kt
+++ src/test/kotlin/edu/carroll/cs389/service/tracker/TrackServiceTest.kt
@@ -1,31 +1,31 @@
 package edu.carroll.cs389.service.tracker
 
-import jakarta.servlet.http.HttpServletRequest
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.test.util.AssertionErrors.assertTrue
 
 
 @SpringBootTest
-class TrackServiceTest(private val trackService: TrackService) {
+class TrackServiceTest {
+
+    @Qualifier("trackServiceRaw")
+    @Autowired
+    private lateinit var trackService: TrackService
 
     // Tests to handle expected input (Happy Path)
     @Test
     fun trackClientTestValidUserAgent() {
-//        val mockRequest: HttpServletRequest = mock(HttpServletRequest::class.java)
-//
-//        `when`(mockRequest.getHeader("User-Agent")).thenReturn("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
-//        `when`(mockRequest.getParameter("requestURI")).thenReturn("/")
-//        `when`(mockRequest.getParameter("requestAddr")).thenReturn("192.168.1.1")
+        val ipv4: String? = "192.168.1.1"
+        val browser: String? = "Firefox"
+        val browserMajorVersion: String? = "110"
+        val platform: String? = "Ubuntu"
+        val platformVersion: String? = "22.04 LTS"
+        val requestURI: String? = "/"
 
-        val mockRequest: MockHttpServletRequest = MockHttpServletRequest()
-        mockRequest.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
-        mockRequest.requestURI = "/"
-        mockRequest.remoteAddr = "192.168.1.1"
-
-        assertTrue("trackClientTestValidUserAgent(): should succeed with valid user agent", trackService.trackClient(mockRequest))
+        assertTrue("trackClientTestValidUserAgent(): should succeed with valid user agent",
+            trackService.trackClient(ipv4, browser, browserMajorVersion, platform, platformVersion,  requestURI))
     }
 
 //    @Test
