b4d678892439fb6a17741f988c7628a123abb0b7
jgrosse01 <jgrosse@carroll.edu>
2023-03-22 17:23:17 -0600

    deployment: application connects with postgres admin user (appuser not working), networked primary to accept connections from docker compose network

--- docker/Grosse_AdvSoft_App/db-primary/docker-entrypoint.sh
+++ docker/Grosse_AdvSoft_App/db-primary/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "host replication all 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
+echo "host all postgres resume-tracker.application_cluster trust" >> "$PGDATA/pg_hba.conf"
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE USER $PG_REP_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_REP_PASSWORD';
